40da26677a735a7f828d4b47e7c74d4d576fcd90
JeanFG <jeanfg86@gmail.com>
2022-01-03 18:31:49 -0300

    Implementacao da Identity WebApp

---
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        [HttpGet("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if(!ModelState.IsValid)
+                return View(usuarioRegistro);
+
+            if (false)            
+                return View(usuarioRegistro);            
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid)
+                return View(usuarioLogin);
+
+            if (false)
+                return View(usuarioLogin); 
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- src/web/NSE.WebApp.MVC/Program.cs
+++ src/web/NSE.WebApp.MVC/Program.cs
@@ -1,27 +1,15 @@
+using NSE.WebApp.MVC.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddIdentityConfiguration();
+
+builder.Services.AddMvcConfiguration();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseMvcConfiguration();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
